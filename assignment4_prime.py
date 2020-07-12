9def5a0fa17e768791459acb0aef81d3eef7659e
AliFEyzi78 <alifeyzi@LAPTOP-DK6C48PF.localdomain>
2020-07-12 20:00:30 +0300

    assignment düzeltme yaptım.

--- assignment4_prime.py
+++ assignment4_prime.py
@@ -3,13 +3,12 @@ if number == 0:
     print(f"{number} is not a prime number!")
 elif number == 1:
     print(f"{number} is not a prime number")
-elif number == 2:
-    print(f"{number} is a prime number")
 else:
-    for i in range (2,number):
-        if number %i == 0:
-            print(f"{number} is not prime number. \n***** '{i}' is divider of {number}*****")
+    for i in range(2, number):
+        if number % i == 0:
+            print(
+                f"{number} is not prime number. \n***** '{i}' is divider of {number}*****"
+            )
             break
     else:
         print(f"{number} is prime number!!")
-        
