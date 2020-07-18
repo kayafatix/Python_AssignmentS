9aecd91b76c2dcec18184f291f2c51c5124ee768
AliFeyzi78 <zeron1g2k@gmail.com>
2020-07-18 13:46:20 +0300

    fibo ve prime ödevi tamamladım.

---
+++ Assignment_6_PrimeNumbers.py
@@ -0,0 +1,12 @@
+n = int(input("Enter a number: "))  
+prime_num = []  
+if n > 1:
+    for j in range(2,n):
+       for i in range(2,j):  
+           if (j % i) == 0:  
+               break  
+       else:  
+           prime_num.append(j)
+else:  
+   print(j,"is not a prime number")
+print(prime_num)
